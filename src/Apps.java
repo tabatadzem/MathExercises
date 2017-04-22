d0b1b766e1160297aa4cada030db0805b5c0b831
tabatadzem <tabatadzem@hotmail.com>
2017-04-22 12:48:52 +0200

    exercies from chepter 2

--- src/Apps.java
+++ src/Apps.java
@@ -1,7 +1,7 @@
 import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 
 import java.text.DecimalFormat;
-import java.util.Scanner;
+import java.util.*;
 
 
 
@@ -47,6 +47,9 @@ public class Apps {
 //        Rectangle rectangle = new Rectangle(width, hight);
 //        System.out.println("area " +df.format( rectangle.getArea()));
 //        System.out.println("perimeter " +df.format( rectangle.getPerimeter()));
+
+
+
 //        double time ;
 //        double distance ;
 //        Scanner scanner = new Scanner(System.in);
@@ -57,20 +60,91 @@ public class Apps {
 //        Runner runner = new Runner( time, distance);
 //        DecimalFormat df = new DecimalFormat("####0.000");
 //        System.out.print("speed is "+df.format( runner.getSpeed() ));
+
+
+//      Population population = new Population(3467849, 3254697,45982 );
 //
+//      System.out.println("first year " + population.getYear1());
+//
+//      System.out.println("first year " + population.getYear2());
+//
+//      System.out.println("first year " + population.getYear3());
+//
+//      System.out.println("first year " + population.getYear4());
+//
+//      System.out.println("first year " + population.getYear5());
 
-      Population population = new Population(3467849, 3254697,45982 );
 
-      System.out.println("first year " + population.getYear1());
+//        double time =1.75 ;
+//        double distance = 24;
+//        double speed;
+//        double mile = 1;
+//        double km;
+//        km = mile/1.6;
+//        speed = distance/time;
+//        DecimalFormat df = new DecimalFormat("####0.000");
+//        System.out.print("runner runs with the speed " +df.format(speed)  + " km/h" );
 
-      System.out.println("first year " + population.getYear2());
 
-      System.out.println("first year " + population.getYear3());
+//        double x, y;
+//        double equation1, equation2;
+//
+//         x = ( 44.5 * 0.55 - 50.2 * 4.9 )/(3.4 *0.55 -2.1 *50.2 );
+//         y = (3.4 * 5.9 - 44.5 * 2.1)/(3.4 * 0.55 - 50.2 * 2.1 );
+//         DecimalFormat df = new DecimalFormat("####0.000");
+//        System.out.print (" & " +df.format(x));
+//        System.out.print (" & " +df.format(y));
 
-      System.out.println("first year " + population.getYear4());
 
-      System.out.println("first year " + population.getYear5());
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a number for radius: ");
+//        double radius = scanner.nextDouble();
+//        double area;
+//        area = radius * radius * 3.14;
+//        System.out.println(area);
+
+//         Scanner scan = new Scanner(System.in);
+//         System.out.println("please enter the value");
+//         double width;
+//         width = scan.nextDouble();
+//         System.out.println("please enter the value");
+//         double height;
+//         height = scan.nextDouble();
+//         double perimeter;
+//         perimeter = 2 *( width + height);
+//         System.out.println("the perimeter is: "+ perimeter);
+
+//         Scanner input = new Scanner(System.in);
+//         System.out.println("please enter the numbers");
+//         int number1 = input.nextInt(), number2 =input.nextInt(),
+//                 number3 = input.nextInt(), number4 = input.nextInt();
+//
+//         double avarage = (number1 + number2 + number3 + number4)/4;
+//         System.out.print(avarage);
+//
+//        int i=1 ,j=i, k=j;
+//
+//        System.out.println(i + " " + j + " " + k);
+
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter a byte value: ");
+//
+//        byte byteValue = input.nextByte();
+//        System.out.print(byteValue);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("please enter the seconds");
+        int seconds = input.nextInt();
+        int minutes = seconds / 60;
+        int  remainingSeconds = minutes%60;
+        System.out.print(minutes + " minutes  and " + remainingSeconds + " seconds ");
+
 
     }
+
+
+
 
 }
